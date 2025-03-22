5b92609e9b4f2d39bd60e30d687e0670162444cf
Abdulrahman Khan <ak47thefirelord@gmail.com>
2025-03-22 19:10:14 -0400

    components and useeffects

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,60 @@
 // web dev simplified tutorial
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList"
 import "./styles.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App(){
-  const [newItem, setNewItem] = useState("");
-  // setNewItem("Hello World");
-  const [todos, setTodos] = useState([]);
+  // because the state is in App.jsx, all our helper functions for managing state are here
+  // pass the functions in as "props" in order to use them in other jsx files
+  const [todos, setTodos] = useState(() =>{
+    const localValue = localStorage.getItem("ITEM")
+    if (localValue==null) return []
+    else return JSON.parse(localValue)
+  });
+  // const [todos, setTodos] = useState([]);
+
+  // use effect hook takes function as argument that runs the function everytime the objects inside the array change
+  // everytime the todos array changes, update the json in localstorage 
+  useEffect(() => {
+    console.log("saved to local storage")
+    localStorage.setItem("ITEM", JSON.stringify(todos))
+  }, [todos])
   
-  // add todo
-  function handleSubmit(e){
-    e.preventDefault(); //prevents page from refreshing
-
-    //everytime we need to update the current value, 
-    // we need to pass it into this function
-    setTodos((currentTodos) => {
-        return [
-          ...currentTodos,
-          {id: crypto.randomUUID(), title: newItem, completed: false}
-        ]
-      }
-    )
-
-    // reset the input box 
-    setNewItem("");
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
   }
-
   //toggle todo checkbox
   function toggleTodo(id, completed) {
     setTodos(currentTodos => {
-      return (currentTodos.map(todo => {
-          if(todo.id === id){
-            return {...todo, completed}
-          }
-
-          return todo
-        }))
-      }
-    )
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
   }
-
-  // delete todo
-  function deleteTodo(id){
+  // Deletes item from todolist
+  function deleteTodo(id) {
     setTodos(currentTodos => {
       return currentTodos.filter(todo => todo.id !== id)
-      }
-
-    )
+    })
   }
 
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input 
-            value={newItem} 
-            // value to update the base value
-            onChange={ e=> setNewItem(e.target.value)} 
-            type="text" 
-            id="item"
-          />
-          <button className="btn">Add Items</button>
-        </div>
-      </form>
+      {/* passing prop */}
+      <NewTodoForm onSubmit={addTodo}/>
+
       <h1 className="header">Todo List</h1>
-      <ul className="list">
-        {todos.length === 0 && "No Todos!"}
-        {todos.map(todo => {
-          return (
-            <li key={todo.id}>
-              <label>
-                <input 
-                  type="checkbox" 
-                  checked={todo.completed} 
-                  onChange={e => toggleTodo(todo.id, e.target.checked)}
-                />
-                {todo.title}
-              </label>
-              <button 
-                onClick={ () => deleteTodo(todo.id)} 
-              className="btn btn-danger">Delete</button>
-            </li>
-          )
-        })}
-      </ul>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
 }
